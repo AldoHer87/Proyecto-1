5a9cbd678fd85d4150745dd7bf16bd160e9cd15e
AldoHer87 <aldojhc@gmail.com>
2022-12-16 21:26:30 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import QuoteBox from './Components/QuoteBox'
+import './App.css/'
+
+function App() {
+  
+  return (
+    <div className="App">
+      <div className>
+        <QuoteBox />
+      </div>
+    </div>
+  )
+}
+
+export default App
